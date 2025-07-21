f7bc632ec9bd8e54cbb8622ab51cc0244f327f30
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-21 23:41:34 +0300

    Create and import taskRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import taskRoutes from './routes/task.routes';
 
 dotenv.config();
 
@@ -10,5 +11,8 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+
+// Routes
+app.use('/api/tasks', taskRoutes);
 
 export default app;
