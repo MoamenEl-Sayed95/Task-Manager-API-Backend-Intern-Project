63584d59e8205dcf8b2e2f64d7d3012cda1d8d5f
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-21 16:54:49 +0300

    Import mongoose & connect with database

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import app from "./app";
+import { config } from "./config";
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+dotenv.config();
+
+const PORT = config.port;
+const MONGO_URI = config.mongoUri;
+mongoose.connect(MONGO_URI).then(() => {
+    console.log(`✅ Connected to MongoDB`);
+    app.listen(PORT, () => {
+        console.log(`🚀 Server is running at http://localhost:${PORT}`);
+    });
+}).catch((err) => {
+    console.log(`❌ MongoDB connection error:`, err.message);
+    process.exit(1);
+});
