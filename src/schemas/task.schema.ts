9c99ae3b659814e0f0ec03896d76c4aea0da127f
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-21 23:09:18 +0300

    Edit Schema status:in_progress

--- src/schemas/task.schema.ts
+++ src/schemas/task.schema.ts
@@ -10,6 +10,6 @@ export const createTaskSchema = Joi.object({
 export const updateTaskSchema = Joi.object({
     title: Joi.string().min(1).max(200).optional(),
     description: Joi.string().max(1000).optional(),
-    status: Joi.string().valid("pending", "in_rogress", "completed").optional(),
+    status: Joi.string().valid("pending", "in_progress", "completed").optional(),
     dueDate: Joi.date().iso().optional(),
 });
