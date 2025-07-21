4289331c083bdefb1a79304d0dabbb9f13ff086f
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-21 16:42:09 +0300

    import dotenv & add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
 
 // Middlewares
 app.use(express.json());
+app.use(morgan("dev"));
 
 export default app;
