84013eeff46ab743303a41af9225c3da210ce34f
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-21 16:27:57 +0300

    import Joi & createTaskSchema & updateTaskSchema

---
+++ src/schemas/task.schema.ts
@@ -0,0 +1,15 @@
+import Joi from "joi";
+
+export const createTaskSchema = Joi.object({
+    title: Joi.string().min(1).max(200).required(),
+    description: Joi.string().max(1000).optional(),
+    status: Joi.string().valid("pending", "in_rogress", "completed").optional(),
+    dueDate: Joi.date().iso().optional(),
+});
+
+export const updateTaskSchema = Joi.object({
+    title: Joi.string().min(1).max(200).optional(),
+    description: Joi.string().max(1000).optional(),
+    status: Joi.string().valid("pending", "in_rogress", "completed").optional(),
+    dueDate: Joi.date().iso().optional(),
+});
