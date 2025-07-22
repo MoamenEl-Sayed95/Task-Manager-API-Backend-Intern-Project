0f488bd495eac69714067aaf3b6d0834ce0c0bf3
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-22 17:26:06 +0300

    add task routes and setup middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,25 @@
-import express from "express";
-import morgan from "morgan";
-import dotenv from "dotenv";
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
 import taskRoutes from './routes/task.routes';
+import { notFound } from './middleware/notFound';
+import { errorHandler } from './middleware/errorHandler';
 
 dotenv.config();
 
 const app = express();
 
-
 // Middlewares
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 // Routes
 app.use('/api/tasks', taskRoutes);
+
+// 404 Not Found Middleware
+app.use(notFound);
+
+// Error Handler Middleware
+app.use(errorHandler);
 
 export default app;
