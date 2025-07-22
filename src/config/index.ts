3142c833fd0f4fdc748d19b6ae8c1546587203c0
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-22 14:35:16 +0300

    Edit MONGODB_URI

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const config = {
     port: Number(process.env.PORT) || 3000,
-    mongoUri:process.env.MONGO_URI || "",
+    mongoUri:process.env.MONGODB_URI || "",
     env: process.env.NODE_ENV || "development",
     logLevel: process.env.LOG_LEVEL || "info",
 };
